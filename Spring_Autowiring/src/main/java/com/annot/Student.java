f51f06056d4d473d3ea75406709d565089fd167d
RabinarayanS <rabinarayansahoo9348@gamil.com>
2023-11-03 18:27:25 +0530

    Setter Injection

--- Spring_Autowiring/src/main/java/com/annot/Student.java
+++ Spring_Autowiring/src/main/java/com/annot/Student.java
@@ -3,13 +3,14 @@ package com.annot;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class Student {
-    @Autowired
+
     private  Address address;  //If we add @Autowired in this field it is called property injection
 
     public Address getAddress() {
         return address;
     }
 
+    @Autowired
     public void setAddress(Address address) {
         System.out.println("Setter Injection");
         this.address = address;
