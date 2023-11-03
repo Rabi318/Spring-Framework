d415158ebcf91dfb7cdb38fa80d80bb058d39ea6
RabinarayanS <rabinarayansahoo9348@gamil.com>
2023-11-03 19:07:45 +0530

    Qualifier

--- Spring_Autowiring/src/main/java/com/annot/Student.java
+++ Spring_Autowiring/src/main/java/com/annot/Student.java
@@ -1,16 +1,19 @@
 package com.annot;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Student {
 
+    @Autowired
+    @Qualifier("address2")
     private  Address address;  //If we add @Autowired in this field it is called property injection
 
     public Address getAddress() {
         return address;
     }
 
-    @Autowired
+
     public void setAddress(Address address) {
         System.out.println("Setter Injection");
         this.address = address;
